36ea17fa8501169b967c48a29c85bc971169195a
Christian Reisancho <geniopol@gmail.com>
2021-10-29 17:48:37 -0500

    Agregar archivos de proyecto.

---
+++ S3Reisancho/S3Reisancho/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace S3Reisancho
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string clave = txtClave.Text;
+
+            if (usuario == "estudiante2021" && clave == "uisrael2021")
+            {
+                await Navigation.PushAsync(new ViewDos(usuario, clave));
+            }
+            else
+            {
+
+                await DisplayAlert("Error", "Usuario o clave ingresados incorrectos", "cancel");
+                txtUsuario.Text = "";
+                txtClave.Text = "";
+            }
+
+        }
+
+
+    }
+}
---
+++ S3Reisancho/S3Reisancho/ViewDos.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S3Reisancho
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewDos : ContentPage
+    {
+        private string usuario;
+        private string clave;
+
+     
+
+        public ViewDos(string usuario, string clave)
+        {
+            this.usuario = usuario;
+            this.clave = clave;
+
+            lblusuario.Text = usuario;
+            lblclave.Text = clave;
+            txtusuario.Text = usuario;
+            txtclave.Text = clave;
+        }
+    }
+}
